dad0e60ee1937d342e796f5bedf1b9c778a59107
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-14 23:39:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def my_hash_creator(key, value)
   hash = {key => value}
 end
-# return a hash that includes the key and value parhttps://learn.co/tracks/fswd-prework-2-01/fswd-prework-2-01-fswd-prework/fswd-prework-2-01-fswd-prework-programming-as-conversation-part-4-collection-data-types-array-and-hash/abstracting-work-with-hashes-lab#ameters passed into this method
+# return a hash that includes the key and value parameters passed into this method
 
 
 def read_from_hash(hash, key)
